250e11081897a37455776d8bdff791f02051d8e3
mrkami1 <mashhood.anwar@gmail.com>
2023-01-23 00:13:55 -0500

    cleared commit history

---
+++ README.md
@@ -0,0 +1,4 @@
+# ticketing-system
+A ticket management system used by employees of a company to manage customer support or IT requests
+
+Allows customer support to open tickets/issues to store in the database and admins of the system to have additional controls over managing the tickets.
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/userModel')
+require('dotenv').config();
+
+//Backend
+
+//test
+app.use(cors());
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_LOGIN);
+
+app.post('/api/register', async(req, res) => {
+    console.log(req.body);
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            accountType: 'user'
+        });
+        res.json({ status: '200 OK'});
+    } catch(err) {
+        res.json({ status: 'error', error: 'Email already exists'});
+    }
+    
+})
+
+app.post('/api/login', async(req, res) => {
+    console.log(req.body);
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    
+    if (user) {
+        return res.json({ status: '200 OK', user: true});
+    }
+    else return res.json({ status: 'error', user: false});
+})
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+})
